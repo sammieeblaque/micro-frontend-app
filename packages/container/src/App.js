405fe6ad49c10171721492e68a278a2818713f2e
Samuel Airehrour <sairehrour@gmail.com>
2024-03-20 13:00:17 +0100

    chore: updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <>
-      <div>Hi there</div>
+      <div>Hi there to me and you</div>
       <hr />
       <MarketingApp />
     </>
