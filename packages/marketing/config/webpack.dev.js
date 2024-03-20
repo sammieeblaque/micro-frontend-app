c6ae044cb1a3876c44f814886a57b4833ceaee4e
Samuel Airehrour <sairehrour@gmail.com>
2024-03-20 11:38:26 +0100

    initial commit

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,30 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const common_config = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const dev_config = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(common_config, dev_config);
